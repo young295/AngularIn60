9b8ca786abecf04d39bd050a60c8c94ba44a277f
Jesse Young <jgyoung295@gmail.com>
2018-02-14 23:32:18 -0500

    updates / added data management

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -36,22 +37,27 @@ export class HomeComponent implements OnInit {
   itemCount: number;
   btnText: string = 'Add an item';
   goalText: string = 'My first life goal';
-  goals = ['My first life goal', 'I want to climb a mountain', 'Go ice skating'];
+  goals = [];
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i) {
     this.goals.splice(i, 1);
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
 }
